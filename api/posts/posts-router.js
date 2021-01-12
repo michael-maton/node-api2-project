364ae1e64b8bc1d9b5387d1f93e7f98d26312169
Michael Maton <mjmaton6@gmail.com>
2021-01-12 16:57:28 -0600

    FINALLY WORKING, get requests done

---
+++ api/posts/posts-router.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const Data = require("../db-helpers");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  Data.find()
+    .then((posts) => {
+      res.status(200).json(posts);
+    })
+    .catch((error) => {
+      console.log(error);
+      res
+        .status(500)
+        .json({ error: "The posts information could not be retrieved." });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  Data.findById(id)
+    .then((post) => {
+      console.log(post);
+      if (!post) {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      } else {
+        res.status(200).json(post);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res
+        .status(500)
+        .json({ error: "The post information could not be retrieved." });
+    });
+});
+
+
+
+
+module.exports = router;
