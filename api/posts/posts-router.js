59b571178b891989decd2d9a4ff24d903177483c
Michael Maton <mjmaton6@gmail.com>
2021-01-12 17:09:37 -0600

    GET and DELETE done

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -2,6 +2,9 @@ const express = require("express");
 const Data = require("../db-helpers");
 const router = express.Router();
 
+// GET REQUESTS
+// GET REQUESTS
+// GET REQUESTS
 router.get("/", (req, res) => {
   Data.find()
     .then((posts) => {
@@ -36,7 +39,43 @@ router.get("/:id", (req, res) => {
     });
 });
 
+router.get("/:id/comments", (req, res) => {
+  Data.findPostComments(req.params.id)
+    .then((comment) => {
+      if (comment) {
+        res.status(201).json(comment);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res
+        .status(500)
+        .json({ error: "The post information could not be retrieved." });
+    });
+});
 
-
+// DELETE REQUEST
+// DELETE REQUEST
+// DELETE REQUEST
+router.delete("/:id", (req, res) => {
+  Data.remove(req.params.id)
+    .then((deletedPost) => {
+      if (!deletedPost) {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      } else {
+        res.status(200).json(deletedPost);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ error: "The post could not be removed" });
+    });
+});
 
 module.exports = router;
